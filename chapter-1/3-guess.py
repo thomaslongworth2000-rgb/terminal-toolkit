cf3c30a060c1240898b2e97d9b4bab646ae5d24e
Tom <thomaslongworth2000@gmail.com>
2026-08-26 21:58:58 +0100

    adding the completed guess file

---
+++ chapter-1/3-guess.py
@@ -0,0 +1,38 @@
+difficulty = input("Choose a difficulty level: Easy or Hard? ").strip().lower()
+
+if difficulty == "easy":
+    secret_number = 6
+
+    while True:
+        guess = int(input("Guess a number between 1 and 10: "))
+
+        if guess < 1 or guess > 10:
+            print("Please enter a valid number between 1 and 10.")
+        elif guess < secret_number:
+            print("Too low! Try again.")
+        elif guess > secret_number:
+            print("Too high! Try again.")
+        else:
+            print("You guessed it! The secret number is", secret_number)
+            break
+
+elif difficulty == "hard":
+    secret_number = 55
+
+    while True:
+        guess = int(input("Guess a number between 1 and 100: "))
+
+        if guess < 1 or guess > 100:
+            print("Please enter a valid number between 1 and 100.")
+        elif guess < secret_number:
+            print("Too low! Try again.")
+        elif guess > secret_number:
+            print("Too high! Try again.")
+        else:
+            print("You guessed it! The secret number is", secret_number)
+            break
+
+else:
+    print("Invalid difficulty selected.")
+
+print("You have completed the game! Thank you for playing.")
