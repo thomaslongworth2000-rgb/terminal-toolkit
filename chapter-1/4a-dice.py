cc96c76c1e583c9d00c9fbe4891c2e6fde7273fb
Tom <thomaslongworth2000@gmail.com>
2026-08-31 13:38:50 +0100

    dice roll file

---
+++ chapter-1/4a-dice.py
@@ -0,0 +1,13 @@
+import random 
+
+dice_sides = int(input("Enter the number of sides for the dice: "))
+dice_amount = int(input("Enter the number of dice to roll: "))
+
+total = 0
+
+for i in range(dice_amount):
+    roll = random.randint(1, dice_sides)
+    print("Roll", i + 1, ":", roll) 
+    total += roll
+print("Total:", total)
+
